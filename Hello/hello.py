b047d077774d425e20d427c5c6f9ff3f6a3afdc3
Xiomara2403 <xc.sarrea.2021@alumnos.urjc.es>
2022-02-01 16:49:47 +0100

    Changes

--- Hello/hello.py
+++ Hello/hello.py
@@ -1,3 +1,8 @@
 print("This is my first string")
-# If it is not in github, the file will be in colour red
+# If it is not in GitHub, the file will be in colour red
 print("Again")
+# Be careful with the file that is open next to the run bottom
+# How to set a breakpoint: press next to 1 and space
+# The line bugging is in blue, because it has not been executing. It is waiting you to ask.
+#   Press bug bottom -> Console -> second bottom "Step over"
+
---
+++ Session-01/SumN.py
@@ -0,0 +1,17 @@
+# Function for calculating the sum of the
+# N first integer numbers
+
+def sumn(n):
+    res = 0
+    for i in range(1, n+1):
+        res += i
+    return res
+
+# -- The main program starts here
+print("Sum of the 20 first integers: ", sumn(20))
+print("Sum of the 100, frist integers: ", sumn(100))
+
+# In order to see it step by step inside the function, we press "Step into"
+# And now we will be able to see the variables and values
+# If the function inside is correct, then we press "Step out" and we continuous outside the function
+
